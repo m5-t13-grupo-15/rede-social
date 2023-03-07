3404671de7460f0acd362404d04d26a8745aff59
jrpjuliana <rocha.juliana.pereira@gmail.com>
2023-03-07 19:06:26 -0300

    feat: structure-raw

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView, Response, status
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from .models import Post
+from .serializers import PostSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
-# Create your views here.
+
+class PostView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def get(self, request, pk):
+        ...
+
+    def post(self, request, pk):
+        ...
+
+    def patch(self, request, pk):
+        ...
+
+    def delete(self, request, pk):
+        ...
