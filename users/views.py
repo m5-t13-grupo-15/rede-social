13aace5635934339ce5b281445d9f6c45d277ce8
LucasR <lrigon2904@gmail.com>
2023-03-07 16:34:31 -0300

    adicionado login/register - view de user list/create - criado novo app request para as solicitações de friends/followers - modelo de model/serializer de request criado NESSERIO FINALIZAR UPDATE

--- users/views.py
+++ users/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework import generics
+from .models import User
+from .serializers import UserSerializer
 
-# Create your views here.
+
+class UserView(generics.ListCreateAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
