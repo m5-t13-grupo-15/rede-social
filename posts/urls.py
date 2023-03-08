457cd24f04e9cd3fe4d1aa209ecd7f7d57eb60a3
jrpjuliana <rocha.juliana.pereira@gmail.com>
2023-03-08 19:22:53 -0300

    feat: Funcao like/unlike (views e rotas)

--- posts/urls.py
+++ posts/urls.py
@@ -5,4 +5,11 @@ from . import views
 urlpatterns = [
     path("posts/", views.PostView.as_view()),
     path("posts/<int:post_id>/", views.PostUniqueView.as_view()),
+    path(
+        "posts/<int:post_id>/like/", views.PostUniqueView.as_view({"put": "like_post"})
+    ),
+    path(
+        "posts/<int:post_id>/unlike/",
+        views.PostUniqueView.as_view({"put": "unlike_post"}),
+    ),
 ]
