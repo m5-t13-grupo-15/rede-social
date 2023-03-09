50d9c3ed6b9ee4e616ed713468d57b3c8631066b
jessicacolombo <jessicacolombo384@gmail.com>
2023-03-09 15:53:40 -0300

    add field to the serializer to return the users friends also

--- users/serializers.py
+++ users/serializers.py
@@ -1,14 +1,28 @@
 from rest_framework import serializers
 from .models import User
+from friends.models import Friends
 
 
 class UserSerializer(serializers.ModelSerializer):
+    friends = serializers.SerializerMethodField(read_only=True)
+
+    def get_friends(self, obj):
+        return Friends.objects.filter(owner_id=obj)
+
     def create(self, validated_data) -> User:
         return User.objects.create_user(**validated_data)
 
     class Meta:
         model = User
-        fields = ["id", "username", "email", "password", "created_at", "updated_at"]
+        fields = [
+            "id",
+            "username",
+            "email",
+            "password",
+            "created_at",
+            "updated_at",
+            "friends",
+        ]
 
         read_only_fields = [
             "is_superuser",
