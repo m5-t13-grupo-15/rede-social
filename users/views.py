0d984f6107e4b2c3a095fdc1360b2724e83f5222
LucasR <lrigon2904@gmail.com>
2023-03-09 12:07:20 -0300

    nada

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework import generics
+from friends.models import FriendList
 from .models import User
 from .serializers import UserSerializer
 
